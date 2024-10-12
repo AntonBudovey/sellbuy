fb6d310e4bdf4ae7d3f21e955a1857324848d6d1
anbudo <anton.budovey.sg@gmail.com>
2024-10-12 17:31:28 +0300

    #3 create docker integration and CI-CD

--- src/main/java/ee/taltech/iti03022024backend/Iti03022024BackendApplication.java
+++ src/main/java/ee/taltech/iti03022024backend/Iti03022024BackendApplication.java
@@ -29,77 +29,77 @@ public class Iti03022024BackendApplication {
         SpringApplication.run(Iti03022024BackendApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner startup() {
-
-        return args -> {
-
-            User user = new User();
-            user.setUsername("admin");
-            user.setEmail("email1");
-            user.setPassword("admin");
-            user.setRoles(Set.of(Role.ADMIN_ROLE));
-            userRepository.save(user);
-
-            User user2 = new User();
-            user2.setUsername("admin2");
-            user2.setPassword("admin2");
-            user2.setEmail("email2");
-            user2.setRoles(Set.of(Role.USER_ROLE));
-            userRepository.save(user2);
-
-            Product product = new Product();
-            product.setTitle("test product");
-            product.setSoldOut(false);
-            product.setPrice(10.0);
-
-            productService.createProduct(product, 1L);
-
-            Product product2 = new Product();
-            product2.setTitle("test product2");
-            product2.setSoldOut(false);
-            product2.setPrice(11.0);
-
-            productService.createProduct(product2, 1L);
-
-            Product product3 = new Product();
-            product3.setTitle("test product3");
-            product3.setSoldOut(false);
-            product3.setPrice(12.0);
-
-            productService.createProduct(product3, 2L);
-
-            Review review = new Review();
-            review.setRating(4.0);
-            review.setText("test review");
-
-            reviewService.createReview(review, 1L);
-
-            Review review2 = new Review();
-            review2.setRating(5.0);
-            review2.setText("test review2");
-
-            reviewService.createReview(review2, 1L);
-
-            Review review3 = new Review();
-            review3.setRating(3.0);
-            review3.setText("test review3");
-
-            reviewService.createReview(review3, 2L);
-
-            Review review4 = new Review();
-            review4.setRating(2.0);
-            review4.setText("test review4");
-
-            reviewService.createReview(review4, 3L);
-
-
-
-
-
-
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner startup() {
+//
+//        return args -> {
+//
+//            User user = new User();
+//            user.setUsername("admin");
+//            user.setEmail("email1");
+//            user.setPassword("admin");
+//            user.setRoles(Set.of(Role.ADMIN_ROLE));
+//            userRepository.save(user);
+//
+//            User user2 = new User();
+//            user2.setUsername("admin2");
+//            user2.setPassword("admin2");
+//            user2.setEmail("email2");
+//            user2.setRoles(Set.of(Role.USER_ROLE));
+//            userRepository.save(user2);
+//
+//            Product product = new Product();
+//            product.setTitle("test product");
+//            product.setSoldOut(false);
+//            product.setPrice(10.0);
+//
+//            productService.createProduct(product, 1L);
+//
+//            Product product2 = new Product();
+//            product2.setTitle("test product2");
+//            product2.setSoldOut(false);
+//            product2.setPrice(11.0);
+//
+//            productService.createProduct(product2, 1L);
+//
+//            Product product3 = new Product();
+//            product3.setTitle("test product3");
+//            product3.setSoldOut(false);
+//            product3.setPrice(12.0);
+//
+//            productService.createProduct(product3, 2L);
+//
+//            Review review = new Review();
+//            review.setRating(4.0);
+//            review.setText("test review");
+//
+//            reviewService.createReview(review, 1L);
+//
+//            Review review2 = new Review();
+//            review2.setRating(5.0);
+//            review2.setText("test review2");
+//
+//            reviewService.createReview(review2, 1L);
+//
+//            Review review3 = new Review();
+//            review3.setRating(3.0);
+//            review3.setText("test review3");
+//
+//            reviewService.createReview(review3, 2L);
+//
+//            Review review4 = new Review();
+//            review4.setRating(2.0);
+//            review4.setText("test review4");
+//
+//            reviewService.createReview(review4, 3L);
+//
+//
+//
+//
+//
+//
+//
+//        };
+//    }
 
 }
